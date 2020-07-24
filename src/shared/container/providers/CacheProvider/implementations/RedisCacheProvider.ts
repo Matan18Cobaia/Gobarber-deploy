a7c1b77d08efaa87b740614cdd5afcacc37ad2f6
Matan18 <mateusandriola18@gmail.com>
2020-07-24 17:37:28 -0300

    Fix: Cant inject CacheProvider in CreateUserServic

--- src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -8,7 +8,6 @@ export default class RedisCacheProvider implements ICacheProvider {
   private client: RedisClient;
   constructor() {
     this.client = new Redis(cacheConfig.config.redis);
-
   }
   public async save(key: string, value: any): Promise<void> {
     await this.client.set(key, JSON.stringify(value));
--- src/shared/container/providers/CacheProvider/index.ts
+++ src/shared/container/providers/CacheProvider/index.ts
@@ -1,5 +1,4 @@
 import { container } from "tsyringe";
-import uploadConfig from "@config/upload"
 import RedisCacheProvider from "./implementations/RedisCacheProvider";
 import ICacheProvider from "./models/ICacheProvider";
 
@@ -8,4 +7,4 @@ const providers = {
   redis: RedisCacheProvider,
 }
 
-container.registerSingleton<ICacheProvider>("StorageProvider", providers.redis)
+container.registerSingleton<ICacheProvider>("CacheProvider", providers.redis)
